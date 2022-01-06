76f525a19bba02cee7f8346f00080d2a53ff6de7
Oreilleu <aurelien.picardpabijan@gmail.com>
2022-01-06 22:15:27 +0100

    Début script page product

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,22 @@
+let colors = document.getElementById('colors')
+
 fetch('http://localhost:3000/api/products')
     .then(res => res.json())
     .then(data => {
         console.table(data)
+        document
+            .getElementById('title')
+            .innerText = data[0].name
+
+        document
+            .getElementById('description')
+            .innerText = data[0].description
+
+        let optionOne = document.createElement('option')
+        optionOne.innerText = "vert"
+        colors.appendChild(optionOne).setAttribute('value', 'vert')
+
+        let optionTwo = document.createElement('option')
+        optionTwo.innerText = "blanc"
+        colors.appendChild(optionTwo).setAttribute('value', 'blanc')
     })
