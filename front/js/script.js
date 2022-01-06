d5bcfb9cf011bc365444c01708d2202feb159295
Oreilleu <aurelien.picardpabijan@gmail.com>
2022-01-06 21:08:49 +0100

    boucle dinamyque pour index.html

--- front/js/script.js
+++ front/js/script.js
@@ -1,53 +1,11 @@
-// let items = document.getElementById('items')
-// let a , article, img, h3, p, data, i;
+// Récupère les données de l'API et les ajoutent dans l'index.html
 
-// let recoversObjectKanap = function () {
-//     fetch('http://localhost:3000/api/products')
-//     .then(res => res.json())
-//     .then(data => {
-//         // console.table(data)
-//         for(let i = 0; i < 8; i++) {
-//             img.src = data[i].imageUrl
-//             h3.innerText = data[i].name
-//             p.innerText = data[i].description
-//         }
-//     })
-// }
-
-// let createSectionItems = function () {
-//     a = document.createElement('a');
-//     a.setAttribute('href', './product.html?id=42')
-//     items.appendChild(a);
-
-//     article = document.createElement('article');
-//     a.appendChild(article);
-
-//     img = document.createElement('img');
-//     img.setAttribute('src', '');
-//     article.appendChild(img);
-
-//     h3 = document.createElement('h3');
-//     h3.classList.add('productName');
-//     article.appendChild(h3);
-
-//     p = document.createElement('p');
-//     p.classList.add('productDescription');
-//     article.appendChild(p);
-
-//     recoversObjectKanap()
-// }
-
-// for(let b = 0; b < 8; b++) {
-//     createSectionItems()
-// }
-
-// ---------------------------------------------------------------------------- //
 let items = document.getElementById('items')
-// let createSectionItems = function () {
+
 fetch('http://localhost:3000/api/products')
 .then(res => res.json())
 .then(data => {
-    for(let i = 0; i < 8; i++) {
+    for(let i = 0; i < data.length; i++) {
         a = document.createElement('a');
         a.setAttribute('href', './product.html?id=42')
         items.appendChild(a);
@@ -70,40 +28,4 @@ fetch('http://localhost:3000/api/products')
         article.appendChild(p);
         p.innerText = data[i].description;
     }
-})
-
-
-// }
-
-// ---------------------------------------------------------------------------- //
-
-
-
-
-// for(let i = 0; i < 10; i++) {
-//     createSectionItems()
-// }
-
-// for(let i = 0; i < 10; i++) {
-//     createSectionItems()
-//     recoversObjectKanap()
-// }   
-
-// Création des éléments pour la section "items"
-
-    // if(res.ok) {
-        //     console.log(res.json())
-            
-        // } else {
-        //     console.log('Erreur')
-        // }
-
-    // .then(function(value) {
-
-    // })
-    // .then(function(value) {
-    //     document
-    //         .getElementById('items')
-    //         .innerHTML = '107fb5b75607497b96722bda5b504926'
-    //     console.log(value)
-    // })
+});
